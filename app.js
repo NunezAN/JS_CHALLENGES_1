798b3d54b7317427c1e8289723f645be13506a74
NunezAN <alex101nunez@gmail.com>
2022-07-04 17:44:40 -0500

    q1

---
+++ app.js
@@ -0,0 +1,9 @@
+//Q1
+
+function addition(num1, num2) {
+    return num1 + num2;
+}
+console.log(`addition(3,2) -> ${addition(3,2)}`);
+console.log(`addition(-3,-6) -> ${addition(-3,-6)}`);
+console.log(`addition(7,3) -> ${addition(7,3)}`);
+
