64b7f7f683710c371203f5fc6609e0ad5371488e
NunezAN <alex101nunez@gmail.com>
2022-07-04 17:47:01 -0500

    q2

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 //Q1
 
-function addition(num1, num2) {
-    return num1 + num2;
-}
-console.log(`addition(3,2) -> ${addition(3,2)}`);
-console.log(`addition(-3,-6) -> ${addition(-3,-6)}`);
-console.log(`addition(7,3) -> ${addition(7,3)}`);
+// function addition(num1, num2) {
+//     return num1 + num2;
+// }
+// console.log(`addition(3,2) -> ${addition(3,2)}`);
+// console.log(`addition(-3,-6) -> ${addition(-3,-6)}`);
+// console.log(`addition(7,3) -> ${addition(7,3)}`);
 
+//Q2
+
+function convertHourToSeconds(hours){
+    return hours*3600;
+}
+
+console.log(convertHourToSeconds(24));
